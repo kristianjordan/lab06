eac46d25e5f559724208e7cce501fb9205924791
kristianjordan <krisjord24@gmail.com>
2021-11-11 12:26:08 -0800

    Implementing definitions and tests for Select_And and Select_Or, modifying the spreadsheet to include the print function

---
+++ andOr_test.hpp
@@ -0,0 +1,93 @@
+#ifndef __ADDOR_TEST_HPP__
+#define __ADDOR_TEST_HPP__
+
+#include "spreadsheet.hpp"
+#include "select.hpp"
+#include "gtest/gtest.h"
+using namespace std;
+
+class Select_Test : public Select_Column
+{
+private:
+	bool result;
+public:
+	Select_Test (bool r) : Select_Column(nullptr, "")
+	{
+		result = r;
+	}
+
+	bool select(const Spreadsheet* sheet, int row) const
+	{
+		return result;
+	}
+
+	virtual bool select(const std::string& s) const
+	{
+		return result;
+	}
+
+};
+
+
+/*
+ *  * testing And cases
+ *  * can either be TT TF FT FF
+*/
+
+TEST(AndTest, AndTrueTrueTest)
+{
+	Select_And * select = new Select_And(new Select_Test(true), new Select_Test(true));
+	EXPECT_EQ(select->select(nullptr,0),  true);
+}
+
+TEST(AndTest, AndTrueFalseTest)
+{
+        Select_And * select = new Select_And(new Select_Test(true), new Select_Test(false));
+        EXPECT_EQ(select->select(nullptr,0),  false);
+}
+
+TEST(AndTest, AndFalseTrueTest)
+{
+        Select_And * select = new Select_And(new Select_Test(false), new Select_Test(true));
+        EXPECT_EQ(select->select(nullptr,0),  false);
+}
+
+
+TEST(AndTest, AndFalseFalseTest)
+{
+        Select_And * select = new Select_And(new Select_Test(false), new Select_Test(false));
+        EXPECT_EQ(select->select(nullptr,0),  false);
+}
+
+
+
+/*
+ * testing Or cases
+ * can either be TT TF FT FF
+*/
+TEST(OrTest, OrTrueTrueTest)
+{
+        Select_Or * select = new Select_Or(new Select_Test(true), new Select_Test(true));
+        EXPECT_EQ(select->select(nullptr,0),  true);
+}
+
+TEST(OrTest, OrTrueFalseTest)
+{
+        Select_Or * select = new Select_Or(new Select_Test(true), new Select_Test(false));
+        EXPECT_EQ(select->select(nullptr,0),  true);
+}
+
+TEST(OrTest, OrFalseTrueTest)
+{
+        Select_Or * select = new Select_Or(new Select_Test(false), new Select_Test(true));
+        EXPECT_EQ(select->select(nullptr,0),  true);
+}
+
+TEST(OrTest, OrFalseFalseTest)
+{
+        Select_Or * select = new Select_Or(new Select_Test(false), new Select_Test(false));
+        EXPECT_EQ(select->select(nullptr,0),  false);
+}
+
+#endif // __ADDOR_TEST_HPP__
+
