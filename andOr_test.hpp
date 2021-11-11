7a18721eef16aaf4285827b8485440daf3635a66
kristianjordan <krisjord24@gmail.com>
2021-11-11 12:36:56 -0800

    Adding deallocations

--- andOr_test.hpp
+++ andOr_test.hpp
@@ -38,18 +38,27 @@ TEST(AndTest, AndTrueTrueTest)
 {
 	Select_And * select = new Select_And(new Select_Test(true), new Select_Test(true));
 	EXPECT_EQ(select->select(nullptr,0),  true);
+	
+	// deallocation
+        delete select;
 }
 
 TEST(AndTest, AndTrueFalseTest)
 {
         Select_And * select = new Select_And(new Select_Test(true), new Select_Test(false));
         EXPECT_EQ(select->select(nullptr,0),  false);
+
+	// deallocation
+        delete select;
 }
 
 TEST(AndTest, AndFalseTrueTest)
 {
         Select_And * select = new Select_And(new Select_Test(false), new Select_Test(true));
         EXPECT_EQ(select->select(nullptr,0),  false);
+
+	// deallocation
+        delete select;
 }
 
 
@@ -57,6 +66,9 @@ TEST(AndTest, AndFalseFalseTest)
 {
         Select_And * select = new Select_And(new Select_Test(false), new Select_Test(false));
         EXPECT_EQ(select->select(nullptr,0),  false);
+
+	// deallocation
+        delete select;
 }
 
 
@@ -69,24 +81,39 @@ TEST(OrTest, OrTrueTrueTest)
 {
         Select_Or * select = new Select_Or(new Select_Test(true), new Select_Test(true));
         EXPECT_EQ(select->select(nullptr,0),  true);
+
+	// deallocation
+        delete select;
+
 }
 
 TEST(OrTest, OrTrueFalseTest)
 {
         Select_Or * select = new Select_Or(new Select_Test(true), new Select_Test(false));
         EXPECT_EQ(select->select(nullptr,0),  true);
+
+	// deallocation
+        delete select;
+
 }
 
 TEST(OrTest, OrFalseTrueTest)
 {
         Select_Or * select = new Select_Or(new Select_Test(false), new Select_Test(true));
         EXPECT_EQ(select->select(nullptr,0),  true);
+
+	// deallocation
+	delete select;
+	
 }
 
 TEST(OrTest, OrFalseFalseTest)
 {
         Select_Or * select = new Select_Or(new Select_Test(false), new Select_Test(false));
         EXPECT_EQ(select->select(nullptr,0),  false);
+
+	// deallocation
+	delete select;
 }
 
 #endif // __ADDOR_TEST_HPP__
